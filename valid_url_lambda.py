ce31d1fbe9cf01ee2634bdf5aa09f0f9af24d302
yogabull <yogabull@gmail.com>
2022-01-26 00:12:36 -0600

    Updated valid_url lambda script to return in a RestAPI format.

--- valid_url_lambda.py
+++ valid_url_lambda.py
@@ -1,14 +1,29 @@
 import urllib3
+import json
+
 
 def lambda_handler(event, context):
-    url = event['url']
-    
     http = urllib3.PoolManager()
-    
+    url = event['queryStringParameters'] ['url']
+
+
     try:
         response = http.request('GET', url)
-        print(f'STATUS CODE: {response.status}.')
-        return response.status
+        response_message = "Site reached: " + str(response.status)
+ 
     except:
-        response = 'NOT VALID'
-        return response
+        response_message = 'Site could not be reached.'
+    
+    
+    transactionResponse = {}
+    transactionResponse['url'] = url
+    transactionResponse['response_message'] = response_message
+    
+    response_object = {}
+    response_object['statusCode'] = 200
+    response_object['headers'] = {}
+    response_object['headers']['Content-Type'] = 'application/json'
+    response_object['body'] = json.dumps(transactionResponse)
+        
+    return response_object
+    
